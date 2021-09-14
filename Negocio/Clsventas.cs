057fa6ab9e4b14c7672195439beec79acc295678
Chris <88003200+Chrioo@users.noreply.github.com>
2021-09-13 18:24:06 -0600

    h

--- Negocio/Clsventas.cs
+++ Negocio/Clsventas.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Tienda.Entidades1;
+using Tienda.Negocio;
 namespace Tienda.Negocio
 {
     class Clsventas
     {
+        private readonly int Precio;
+        private int Cantidad;
+
+        public string Paga(Clsventas ventas)
+        {
+            {
+                double total = ventas.Precio * ventas.Cantidad;
+
+                if (ventas.Precio > 50)
+                {
+                    double descuento = total - (total * 0.20);
+                    Console.WriteLine("aplica al 20% de descuento su total es de " + descuento);
+                }
+                else
+                {
+                    Console.WriteLine("no aplica descuento su total es" + total);
+                }
+
+                return "";
+            }
+        }
+
+        internal bool Cobro(ventas vent)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+//hecho por Critian y maria jose
